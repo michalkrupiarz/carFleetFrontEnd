7fc754160e8aad8c5aa60fabb12de9648f36a72b
Michal Krupiarz <michal.krupiarz@redknee.com>
2017-01-23 14:47:22 +0100

    working routing

--- src/main/webapp/js/car/carController.js
+++ src/main/webapp/js/car/carController.js
@@ -2,14 +2,14 @@
 	'use strict';
 	angular.module('app.carController',['app.carService'])
 		.controller ('carController',carController);
-		carController.$inject= ['carService', '$scope'];
+		carController.$inject= ['carService'];
 		
 		function carController(carService, $scope){
 			var vm = this;	
 			vm.cars = [];
 		    vm.getCars = function(){
 				carService.gAC().then(function(r){
-					console.log(r);
+					
 					vm.cars=r;
 				})
 			}
